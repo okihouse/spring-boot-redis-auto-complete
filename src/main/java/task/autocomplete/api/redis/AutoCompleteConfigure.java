cd4fbebb25d36c44559c7bb4437c4e5d93650400
OKIHOUSE <okihouse@brace.xyz>
2015-12-08 14:50:59 +0900

    update redis configuration with propertis binding.

--- src/main/java/task/autocomplete/api/redis/AutoCompleteConfigure.java
+++ src/main/java/task/autocomplete/api/redis/AutoCompleteConfigure.java
@@ -1,24 +1,21 @@
 package task.autocomplete.api.redis;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Component
 @Data
+@ConfigurationProperties(prefix="autocomplete")
 public class AutoCompleteConfigure {
 
-	@Value(value = "${autocomplete.min}")
 	private double min = 0;
 	
-	@Value(value = "${autocomplete.max}")
 	private double max = 5;
 
-	@Value(value = "${autocomplete.count}")
 	private long count = 30;
 	
-	@Value(value = "${autocomplete.delemeter}")
 	private final String delemeter = "§";
 
 }
